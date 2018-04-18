c7eba555d26c6a564bec3956f5d7f5db07b03cf3
Greg Orvik <gvorvik@gmail.com>
2018-04-18 16:43:23 -0500

    display index.html on port

--- server/server.js
+++ server/server.js
@@ -2,3 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+const monsters = ['Big Foot', 'Loch Ness Monster', 'Mike', 'Sully'];
+
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}.`);
+});
