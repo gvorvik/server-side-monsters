0d26fa5f1c41b71ec38a12cbf24bcbd45d48710b
Greg Orvik <gvorvik@gmail.com>
2018-04-18 17:11:42 -0500

    add monsters to dom on load

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,17 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery is ready');
-    
+    loadMonsters();   
+}
+
+function loadMonsters() {
+    $.ajax({
+        type: "GET",
+        url: '/monsters'
+    })
+    .then((response) => {
+        for (let monster of response) {
+            $('#listOfMonsters').append(`<li>${monster}</li>`);
+        }
+    });
 }
